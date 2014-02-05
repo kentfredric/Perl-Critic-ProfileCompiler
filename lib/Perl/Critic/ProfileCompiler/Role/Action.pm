797d668bb29e29cd44c4745f26c40bf0586ac941
Kent Fredric <kentfredric@gmail.com>
2014-02-05 19:28:43 +1300

    +::Role::Action

---
+++ lib/Perl/Critic/ProfileCompiler/Role/Action.pm
@@ -0,0 +1,44 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package Perl::Critic::ProfileCompiler::Role::Action;
+
+# ABSTRACT: Basic interface spec for Action objects
+
+# AUTHORITY
+
+use Moo::Role;
+
+has action_name => (
+  is      => ro =>,
+  lazy    => 1,
+  builder => 1,
+);
+
+sub _build_action_name {
+  my ($self) = @_;
+  require Scalar::Util;
+  my $class = blessed($self);
+  if ( $class =~ /^Perl::Critic::ProfileCompiler::Action::(.+)$/ ) {
+    return "$1";
+  }
+  return $class;
+}
+
+sub _inline {
+  my ( $self, $stack ) = @_;
+  return $stack unless $self->can('inline');
+  return $self->inline($stack);
+}
+
+sub _apply {
+  my ( $self, $config ) = @_;
+  return $config unless $self->can('apply');
+  return $self->apply($config);
+}
+
+no Moo::Role;
+1;
+
