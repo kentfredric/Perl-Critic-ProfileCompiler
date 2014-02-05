a7f982eb1e3931688964aec9a34ee1f5778bd144
Kent Fredric <kentfredric@gmail.com>
2014-02-05 19:44:33 +1300

    +Role::Bundle::Simple

---
+++ lib/Perl/Critic/ProfileCompiler/Role/Bundle/Simple.pm
@@ -0,0 +1,27 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package Perl::Critic::ProfileCompiler::Role::Bundle::Simple;
+
+# ABSTRACT: A role for bundles that are just collections to policies with no advanced mechanics
+
+# AUTHORITY
+
+use Moo::Role;
+
+with 'Perl::Critic::ProfileCompiler::Role::Bundle';
+
+requires 'policies';
+
+sub configure {
+  my ($self) = @_;
+  for my $policy ( $self->policies ) {
+    $self->add_or_replace_plugin($policy);
+  }
+}
+
+no Moo::Role;
+1;
+
